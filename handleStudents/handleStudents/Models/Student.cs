24d3418dc709de681d1c3d9c8ab0c40464de88f2
Heber Choque <christian.choque.c@gmail.com>
2020-03-22 12:52:10 -0400

    Added model of student with its enums of school and gender

---
+++ handleStudents/handleStudents/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace handleStudents.Models
+{
+    public enum StudentType
+    {
+        kinder,
+        elementary,
+        high,
+        university
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+
+    public class Student
+    {
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        public StudentType  StudentType { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
