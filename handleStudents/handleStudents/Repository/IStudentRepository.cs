8dea512ae52472829a882a2252d484bb4345b507
Heber Choque <christian.choque.c@gmail.com>
2020-03-22 18:42:12 -0400

    Implemented repository patten to add operations of student model  like add student or delete student

---
+++ handleStudents/handleStudents/Repository/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using handleStudents.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace handleStudents.Repository
+{
+    public interface IStudentRepository
+    {
+        public Student AddNewStudent( Student student);
+        public Student GetStudent();
+        public IEnumerable<Student> GetAllStudents();
+        public Boolean DeleteStudent(Guid id);
+        public IEnumerable<Student> GetStudentsByName( string name);
+        public IEnumerable<Student> GetStudentsByTypeOfSchool(string school);
+        public IEnumerable<Student> GetStudentsByGender(string gender);
+    }
+}
---
+++ handleStudents/handleStudents/Repository/StudentRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using handleStudents.Models;
+
+namespace handleStudents.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public static List<Student> _students;
+
+        public StudentRepository()
+        {
+            _students = new List<Student>();
+        }
+
+        public bool DeleteStudent(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _students;
+        }
+
+        public Student GetStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsByGender(string gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsByTypeOfSchool(string school)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student AddNewStudent(Student student)
+        {
+            student.EnrollmentDate = DateTime.Now;
+            _students.Add(student);
+            return student;
+        }
+    }
+}
